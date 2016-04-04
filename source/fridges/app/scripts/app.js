ee5ef08ccc30cf9f4c3b162edb4918549e44fb79
Mike Hewitson <mike@hewitson.co.za>
2016-04-04 14:23:17 +0200

    Add outline for front end

---
+++ source/fridges/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fridgesApp
+ * @description
+ * # fridgesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fridgesApp', [
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
