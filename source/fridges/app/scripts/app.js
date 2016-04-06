ff889d1570d386cd549c841543514cc58b318d7c
Mike Hewitson <mike@hewitson.co.za>
2016-04-06 13:35:44 +0200

    Got grapgh working

--- source/fridges/app/scripts/app.js
+++ source/fridges/app/scripts/app.js
@@ -9,30 +9,31 @@
  * Main module of the application.
  */
 angular
-  .module('fridgesApp', [
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/history', {
-        templateUrl: 'views/history.html',
-        controller: 'HistoryCtrl',
-        controllerAs: 'history'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('fridgesApp', [
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'googlechart'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .when('/history', {
+                templateUrl: 'views/history.html',
+                controller: 'HistoryCtrl',
+                controllerAs: 'history'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- source/fridges/app/scripts/controllers/history.js
+++ source/fridges/app/scripts/controllers/history.js
@@ -21,4 +21,43 @@ angular.module('fridgesApp')
                 $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
             });
 
+    }])
+    .controller('HistoryChartCtrl', ['$scope', 'historyFactory', function($scope, historyFactory) {
+
+        $scope.showData = false;
+        $scope.message = 'Loading ...';
+        historyFactory.getReadings().query(
+            function(response) {
+                $scope.readings = response;
+                $scope.showData = true;
+                $scope.rows = [];
+
+                for (var reading of $scope.readings) {
+                    $scope.rows.push({ c: [{ v: new Date(reading.date) }, { v: reading.sensors[0].temp }, { v: reading.sensors[0].hum }] });
+                }
+
+                console.log($scope.rows);
+
+                $scope.chartObject = {};
+
+                $scope.chartObject.type = 'LineChart';
+
+                $scope.chartObject.data = {
+                    'cols': [
+                        { id: 't', label: 'Topping', type: 'string' },
+                        { id: 's', label: 'Temperature', type: 'number' },
+                        { id: 's', label: 'Humidity', type: 'number' }
+                    ],
+                    'rows': $scope.rows 
+                };
+
+                $scope.chartObject.options = {
+                    'title': 'Ambient'
+                };
+
+            },
+            function(response) {
+                $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
+            });
+
     }]);
