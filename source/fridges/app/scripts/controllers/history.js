15891e27cc56bed7dd2430f3cfb149b9e7feebc4
Mike Hewitson <mike@hewitson.co.za>
2016-04-06 16:24:05 +0200

    Get three graphs working

--- source/fridges/app/scripts/controllers/history.js
+++ source/fridges/app/scripts/controllers/history.js
@@ -1,5 +1,53 @@
 'use strict';
 
+function getSensorByName(name, sensors) {
+    return sensors.filter(
+        function(sensor) {
+            return sensor.sensor === name;
+        }
+    );
+}
+
+function buildRows(sensor, readings) {
+    var rows = [];
+
+    for (var reading of readings) {
+        var data = getSensorByName(sensor, reading.sensors)[0];
+        rows.push({ c: [{ v: new Date(reading.date) }, { v: data.temp }, { v: data.hum }] });
+    }
+    return rows;
+}
+
+function buildChart(sensor, readings) {
+    var chartObject = {};
+    var cols = [
+        { id: 't', label: 'Date', type: 'string' },
+        { id: 's', label: 'Temperature', type: 'number' },
+        { id: 's', label: 'Humidity', type: 'number' }
+    ];
+    var rows = buildRows(sensor, readings);
+    var title = sensor + ' Conditions';
+    chartObject.type = 'LineChart';
+    chartObject.data = {
+        'cols': cols,
+        'rows': rows
+    };
+
+    chartObject.options = {
+        'title': title,
+        series: {
+            0: { targetAxisIndex: 0, type: 'line' },
+            1: { targetAxisIndex: 1, type: 'line' }
+        },
+        vAxes: [
+            { title: 'Degrees C' },
+            { title: 'Percentage' }
+        ]
+    };
+    return chartObject;
+
+}
+
 /**
  * @ngdoc function
  * @name fridgesApp.controller:MainCtrl
@@ -30,30 +78,12 @@ angular.module('fridgesApp')
             function(response) {
                 $scope.readings = response;
                 $scope.showData = true;
-                $scope.rows = [];
 
-                for (var reading of $scope.readings) {
-                    $scope.rows.push({ c: [{ v: new Date(reading.date) }, { v: reading.sensors[0].temp }, { v: reading.sensors[0].hum }] });
-                }
+                // Build chart objects
+                $scope.chartObject1 = buildChart('Ambient', $scope.readings);
+                $scope.chartObject2 = buildChart('Curing', $scope.readings);
+                $scope.chartObject3 = buildChart('Fridge', $scope.readings);
 
-                console.log($scope.rows);
-
-                $scope.chartObject = {};
-
-                $scope.chartObject.type = 'LineChart';
-
-                $scope.chartObject.data = {
-                    'cols': [
-                        { id: 't', label: 'Topping', type: 'string' },
-                        { id: 's', label: 'Temperature', type: 'number' },
-                        { id: 's', label: 'Humidity', type: 'number' }
-                    ],
-                    'rows': $scope.rows 
-                };
-
-                $scope.chartObject.options = {
-                    'title': 'Ambient'
-                };
 
             },
             function(response) {
